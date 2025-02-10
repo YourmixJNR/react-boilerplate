38f0a64aa88ef3079ae4f3c03c41a2b9e0de6ee6
YourmixJNR <radmakson@gmail.com>
2025-02-10 03:15:22 +0100

    :sparkle: feat: update index.html title and meta description; add Google Fonts preconnect links; integrate vite-plugin-sitemap in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Sitemap({
+      hostname: "https://linktowebsite.com",
+      dynamicRoutes: ["/", "/about"],
+      // add more routes in the dynamic routes array
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
